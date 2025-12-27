3a6908d6722f31c8ea65cf6a56b72b532c8745ac
Micaias Carvalho Vieira <73481473+micaiasdev@users.noreply.github.com>
2025-12-27 11:15:00 -0300

    next-version-13.1.6

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
 function Home() {
-  return <h1>Teste7</h1>
+  return (
+    <h1>
+      Não há nada que você possa fazer para Deus te amar mais; e não há nada que
+      <br></br>
+      você possa fazer para Deus te amar menos.
+      <p>
+        <strong>Philip Yancey</strong>
+      </p>
+    </h1>
+  )
 }
 
 export default Home
