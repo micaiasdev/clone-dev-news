42411a9b9d4f3a4b24b2144c7169fb7e79de035e
Micaias <miciaiasvieira@gmail.com>
2026-03-21 23:16:27 -0300

    review until the 19th, ended

--- infra/database.js
+++ infra/database.js
@@ -1,20 +1,19 @@
-import { Client } from 'pg';
+import { Client } from "pg";
 
 async function query(queryObject) {
   const client = new Client({
     host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-
-  })
-  await client.connect()
-  const result = await client.query(queryObject)
-  await client.end()
-  return result
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+  });
+  await client.connect();
+  const result = await client.query(queryObject);
+  await client.end();
+  return result;
 }
 
 export default {
- query: query,
-}
+  query: query,
+};
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,8 @@
-import database from "infra/database.js"
+import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query('SELECT 1 + 1 as sum;')
-  console.log(result.rows)
+  const res = await database.query("SELECT 1 + 1;");
+  console.log(res.rows);
   response.status(200).json({ Chave: "FOI MASSSA OH" });
 }
 export default status;
