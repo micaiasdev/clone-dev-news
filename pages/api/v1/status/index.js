41b389bbdc40b34696ff4bdb5b14075eb82725d0
Micaias <miciaiasvieira@gmail.com>
2026-03-29 16:56:58 -0300

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,29 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const res = await database.query("SELECT 1 + 1;");
-  console.log(res.rows);
-  response.status(200).json({ Chave: "FOI MASSSA OH" });
+  const updateAt = new Date().toISOString();
+
+  const queryVersion = await database.query("SHOW server_version;");
+  const postgresVersion = queryVersion.rows[0].server_version;
+
+  const queryMaxConnections = await database.query("SHOW max_connections");
+  const postgresMaxConnections = queryMaxConnections.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB;
+  const queryActiveConnections = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const activeConnections = queryActiveConnections.rows[0].count;
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: postgresVersion,
+        max_connections: postgresMaxConnections,
+        active_connections: activeConnections,
+      },
+    },
+  });
 }
 export default status;
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,4 +1,27 @@
 test("GET in /api/v1/status shold be return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  // Verify if is defined
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+  // Verify Value
+  const parseDateResponseBody = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toBe(parseDateResponseBody);
+  // database version
+  const postgresVersion = responseBody.dependencies.database.version;
+  expect(postgresVersion).toBeDefined();
+  const regexVersion = new RegExp("[0-9][0-9].[0-9]([0-9])");
+  expect(postgresVersion).toMatch(regexVersion);
+  // max connections
+  const maxConnections = responseBody.dependencies.database.max_connections;
+  expect(maxConnections).toBeDefined();
+  const parseMaxConnections = maxConnections;
+  expect(parseMaxConnections).not.toBeNaN();
+  //active connections
+  const activeConnections =
+    responseBody.dependencies.database.active_connections;
+  expect(activeConnections).toBeDefined();
+  const parseActiveConnections = Number(activeConnections);
+  expect(parseActiveConnections).not.toBeNaN();
+  expect(parseActiveConnections).toBe(1);
 });
