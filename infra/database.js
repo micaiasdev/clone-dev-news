4d4187a4add5089afa3fb7900ef0323817fe1cb4
Micaias <miciaiasvieira@gmail.com>
2026-04-11 19:13:32 -0300

    ignore depreceated use baseURl and remove database console.log credentials

--- infra/database.js
+++ infra/database.js
@@ -8,13 +8,6 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     database: process.env.POSTGRES_DB,
   });
-  console.log("Credenciais do Postgres", {
-    host: process.env.POSTGRES_HOST,
-    user: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    port: process.env.POSTGRES_PORT,
-    database: process.env.POSTGRES_DB,
-  });
   try {
     await client.connect();
     const result = await client.query(queryObject);
